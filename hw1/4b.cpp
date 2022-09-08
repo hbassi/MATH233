b3932d2daf4648273bdbd714f57dd6e80c0eb296
Hardeep Bassi <hbassi21@berkeley.edu>
2022-09-08 12:44:45 -0700

    power function bugged

--- hw1/4b.cpp
+++ hw1/4b.cpp
@@ -25,7 +25,7 @@ int main ()
     else {
         double* sampled = sampledLegendre(a,b,N, n);
         for (int i = 0; i < N; i++) {
-            //todo: display the linspace
+
             std::cout << "sL[" << i << "] : ";
             std::cout << *(sampled + i) << std::endl;
         }
@@ -33,28 +33,15 @@ int main ()
     }
 
 }
-double power(double num, int pow) {
-    switch(pow) {
-        case 0: return 1;
-        case 1: return num;
-        case 2: return num*num;
-        default: {
-            double val = num;
-            for (int i = 0; i < pow; i++) {
-                val *= num;
-            }
-            return val;
-        }
-    }
-}
+
 double Legendre(double x, int n) {
     if (n == 0) {return 1;}
     else if (n == 1) {return x;}
-    else if (n == 2) {return (0.5)* (3*power(x,2) - 1);}
-    else if (n == 3) {return (0.5)*(5*power(x,3) - 3*x);}
-    else if (n == 4) {return (0.125)* (35*power(x,4) - 30*power(x,2)+3);}
-    else if (n == 5) {return (0.125) * (63*power(x,5) - 70* power(x,3) + 15*x);}
-    else {return (0.0625) * (231*power(x,6) - 315* power(x,4) + 105* power(x,2) - 5);}
+    else if (n == 2) {return (0.5)* (3*x*x - 1);}
+    else if (n == 3) {return (0.5)*(5*x*x*x - 3*x);}
+    else if (n == 4) {return (0.125)* (35*x*x*x*x - 30*x*x+3);}
+    else if (n == 5) {return (0.125) * (63*x*x*x*x*x - 70*x*x*x + 15*x);}
+    else {return (0.0625) * (231*x*x*x*x*x*x - 315*x*x*x*x + 105*x*x - 5);}
 }
 //https://www.tutorialspoint.com/cplusplus/cpp_return_arrays_from_functions.htm return pointer to array
 double* sampledLegendre(double a, double b, int N, int n) {
